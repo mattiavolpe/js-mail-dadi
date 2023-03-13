a80406c8d3582a99eb64bd3f96f18a113dc4f1bc
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-13 14:28:46 +0100

    INIT with basic assets

---
+++ mail/assets/js/main.js
@@ -0,0 +1,6 @@
+/*
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Mail Bonus
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+*/
